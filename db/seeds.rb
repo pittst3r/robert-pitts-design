ed84dbcf62b4815fa09796f9b1328633af1af560
Robbie Pitts <robbie@sweatypitts.com>
2012-11-12 20:59:43 -0500

    Now with responsiveness.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Skill.all.delete
+16.times do
+  s = Skill.create(name: Faker::Lorem.words(2).each(&:upcase).join(" "), description: Faker::Lorem.paragraph)
+  puts "'#{s.name}' created."
+end
