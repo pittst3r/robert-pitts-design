dc8a682fb355a49b86a65b3a8c2159943d701d7e
Robbie Pitts <robbie@sweatypitts.com>
2012-11-16 13:06:22 -0500

    More seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Skill.all.delete
 # 16.times do
-#   s = Skill.create(name: Faker::Lorem.words(2).each(&:upcase).join(" "), description: Faker::Lorem.paragraph)
+#   s = Skill.create(name: Faker::Lorem.words(2).each(&:capitalize!).join(" "), description: Faker::Lorem.paragraph)
 #   puts "'#{s.name}' created."
 # end
 Skill.create(
@@ -26,6 +26,42 @@ Skill.create(
 Skill.create(
   name: "jQuery",
   description: "jQuery made javascript fun for me again. When I was young and green, unobtrusive javascript was not really a concern of mine. However, as I grew my craft and became increasingly particular about my markup, writing unobtrusive javascript was just a pain in the ass. I felt like I was always just <em>getting things ready</em> to make things happen rather than <em>actually</em> making things happen. jQuery fixed this. Not only does it make javascript fun, it makes it much faster to write."
+)
+Skill.create(
+  name: "JavaScript",
+  description: "I was using JavaScript from the beginning. When I was twelve I used it to make the page snow and have text follow the cursor around like a snake. Nowadays, I use it to actually improve the user experience, not just to impress my friends."
+)
+Skill.create(
+  name: "HAML",
+  description: "My favorite templating language. It looks beautiful and works great. <3"
+)
+Skill.create(
+  name: "SASS",
+  description: "I use SASS instead of SCSS. No more pesky brackets and semicolons for me. I like the enforced indentation structure&mdash;it makes it easier to write pretty code. Oh yeah, mixins and variables are cool too."
+)
+Skill.create(
+  name: "HTML5",
+  description: "Yes, I am familiar with the new tags, redefined tags, tag attributes, etc. People often confuse the features of HTML5 with what is actually the features of the combination of HTML5, CSS3, and JavaScript."
+)
+Skill.create(
+  name: "CSS3",
+  description: "Having spent most of my career pre-CSS3, I've spent a lot of time having to conjure up work-arounds because of CSS's past limitations. I'm quite excited about CSS3. Woohoo!"
+)
+Skill.create(
+  name: "BDD",
+  description: "At my previous company, Flagstuff.com, we hired a Rails firm called Hashrocket to make the latest iteration of the site along with a very, very robust \"back office\" system. They used BDD and that is where I got to learn first-hand how it works in the real world and with a very large project. Communicating and prioritizing features and bugs to the developers was very easy. Some argue that the tedium of writing and maintaining the tests and step definitions outweighs the benefits, but I submit that the time is worth it if it will keep the project focused and on track."
+)
+Skill.create(
+  name: "Photoshop",
+  description: "I've been using Photoshop from the beginning when I was 13. It's what I use to make mockups and bitmap graphics."
+)
+Skill.create(
+  name: "Illustrator",
+  description: "Illustrator is a more recent skill for me. I began using it initially to design custom flags for customers (which sometimes had very intricate designs) and then started to design various print materials for Flagstuff.com and other businesses."
+)
+Skill.create(
+  name: "Git",
+  description: "I obviously know how to use Git. Please see my GitHub profile for proof."
 )
 
 Curriculum.all.delete
