62581587f336ff90216001f7dc18b44ee5d11afb
Robbie Pitts <robbie@sweatypitts.com>
2013-04-04 19:54:42 -0400

    Added tumblr rss feed.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,22 @@
+require 'rss'
+require 'open-uri'
+
 class PagesController < ApplicationController
   expose(:skills) { Skill.asc(:name_lowercase) }
   expose(:curricula) { Curriculum.desc(:from_date, :to_date) }
   expose(:commits) {
     client = Octokit::Client.new(login: ENV['GITHUB_USERNAME'], password: ENV['GITHUB_PASSWORD'])
     client.commits("sweatypitts/robert-pitts-design")
+  }
+  expose(:posts) {
+    url = 'http://sweatypitts.tumblr.com/rss'
+    posts = []
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      feed.items.each do |item|
+        posts << Hashie::Mash.new(title: item.title, body: item.description, link: item.link, date: DateTime.parse(item.pubDate.to_s).strftime('%F'))
+      end
+    end
+    posts
   }
 end
